11cfdd6f7c45330b2d1004006a97ab2bc07f458f
Muskan Chawla <muskanchawla63@gmail.com>
2026-03-20 19:19:11 +0100

    final commit for M2-fixing colour visibility

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Group 33</h1>
+        <h1 style={{color: "red"}}>Group 33</h1>
         <Image
           className={styles.logo}
           src="/next.svg"
