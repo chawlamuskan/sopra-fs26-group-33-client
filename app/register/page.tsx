cdd3f9240f4fd82280d52c1cf5a5a6c865932e61
nsavas8 <nina.jael@gmx.ch>
2026-04-15 16:18:52 +0200

    Add redirect to preferences after successful registration
    Redirect user to preferences page after registration.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -25,6 +25,7 @@ const Register: React.FC = () => {
         setToken(response.token);
         localStorage.setItem("user", JSON.stringify(response));
       }
+      router.push("/preferences");
     } catch (error) {
       if (error instanceof Error) {
         alert(`Something went wrong during the registration:\n${error.message}`);
@@ -120,7 +121,7 @@ const Register: React.FC = () => {
               Already registered? Login
             </button>
 
-            <button type="submit" className={styles.btnLogin} style={{ width: "179px" }} onClick={() => router.push("/preferences")}>
+            <button type="submit" className={styles.btnLogin} style={{ width: "179px" }}>
               Save profile
             </button>
           </div>
